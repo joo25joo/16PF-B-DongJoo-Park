dcc90a294fb163db76df18816d96e518c967c560
박동주 <joo25joo@naver.com>
2016-03-11 15:22:27 +0900

    ex01.py 샵 삭제

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,11 +1,11 @@
 print("Hello World")
-# print("Hello Again")
-# print "I like typing this"
-# print "this is fun"
-# print "Yay! Printing"
-# print "I'd much rather you 'not'."
-# print 'I "said" do not touch this'
-#
-# print "Dongjoo Park"
-# print "16 03 11"
-# print "Pip-boy 3000"
+ print("Hello Again")
+ print "I like typing this"
+ print "this is fun"
+ print "Yay! Printing"
+ print "I'd much rather you 'not'."
+ print 'I "said" do not touch this'
+
+ print "Dongjoo Park"
+ print "16 03 11"
+ print "Pip-boy 3000
