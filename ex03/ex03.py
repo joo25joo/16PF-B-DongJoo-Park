7afb7f3dbf5508d1992a06711f7c0bb5ab5e4b48
DongJoo Park <joo25joo@naver.com>
2016-03-18 15:34:44 +0900

    ex03.py

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -19,6 +19,6 @@ print "Oh, that's why it's False."
 
 print "How about some more."
 
-print "Is it greater?", 5 > -2
+print "Is it greater?", 5 < -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -3
