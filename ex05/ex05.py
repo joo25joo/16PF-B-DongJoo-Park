5caf5147e6c7b8f40fabe9c7c5d1b322ac2b2eac
Park Dongjoo <joo25joo@naver.com>
2016-04-01 15:16:32 +0900

    Fin

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,14 +1,17 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height_cm = 180
-my_weight_kg = 80
-my_eyes = 'Blue'
+my_name = 'DongJoo Pakr'
+my_age = 23 # not a lie
+my_height_cm = 175
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
+my_weight_kg = 56
+my_eyes = 'brown'
 my_teeth = 'White'
-my_hair = 'Brown'
+my_hair = 'deep dark brown'
 
 print "Let's talk about %s." % my_name
-print "He's %d cm tall." % my_height_cm
-print "He's %d kg heavy." % my_weight_kg
+print "He's %g cm tall." % my_height_cm
+print "He's %g inches tall" % my_height_inch
+print "He's %g kg heavy." % my_weight_kg
 print "Actually that's not too heavy"
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffe" % my_teeth
