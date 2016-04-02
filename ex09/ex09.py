d0ec9a9aeb8187baacf38cce63d34cd39a7947bf
Dongjoo Park <joo25joo@naver.com>
2016-04-02 11:07:15 +0900

    ""

--- ex09/ex09.py
+++ ex09/ex09.py
@@ -1,7 +1,7 @@
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan/Feb/Mar/Apr/May/Jun/Jul/Aug"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days: ", days
 print "Here are the months: ", months
