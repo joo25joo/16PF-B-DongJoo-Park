a6dd88560885fc02c1f2caa30b79f6560410d225
CAD Client <CAD Client>
2016-05-13 13:54:47 +0900

    entered ex25.py contents

---
+++ ex28/ex28.py
@@ -0,0 +1,11 @@
+# coding: ut f-8
+# ckarhansgjs : http://learnpythonthehardway.or/book/ex28.html
+
+print(" 1. True and True = %s" % str(True and True))
+print(" 2. False and True = %s" % str(False and True))
+print(" 3. 1==1 and 2==1 = %s" % ((1 == 1) and (2 == 1)))
+print(" 4. 'test' == 'test' = %s" % str('test' == 'test'))
+print(" 5. 1==1 or 2!=1 = %s" % ((1 == 1) or (2 != 1)))
+print(" 6. True and 1==1 = %s" % str(True and (1 == 1)))
+print(" 7. False and 0!=0 = %s" % str(False and (0 != 0)))
+print(" 8. True or 1==1 = %s" % Ture or (1 == 1))
