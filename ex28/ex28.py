2295dd05b39c20d55303cc575d17e8f4b6f5d0c4
CAD Client <CAD Client>
2016-05-13 15:15:02 +0900

    cococommmmmmiiittttcomitcommit

--- ex28/ex28.py
+++ ex28/ex28.py
@@ -1,4 +1,4 @@
-# coding: ut f-8
+# coding: utf-8
 # ckarhansgjs : http://learnpythonthehardway.or/book/ex28.html
 
 print(" 1. True and True = %s" % str(True and True))
@@ -23,11 +23,10 @@ print("18. 1==1 and not('testing'==1 or 1==0) = %s" % (
     (1 == 1) and not (('testing' == 1) or (1 == 0))))
 print("19. 'chnky'=='bacon' and not (3==4 or 3==3) = %s" % (
     ('chunky' == 'bacon') and not ((3 == 4) or (3 == 3))))
-print("20. 3==3 and not('testing'=='testing' or 'Python'=='Fun') = %s" (
+print("20. 3==3 and not('testing'=='testing' or 'Python'=='Fun') = %s" % (
     (3 == 3) and not (('testing' == 'testing') or ('Python' == 'Fun'))))
 
 print("extra 1. 'test' and 'test' = = %s" % str('test' and 'test'))
 print("extra 2. 1 and 1 = = %s" % str(1 and 1))
 print("extra 3. False and 1 = = %s" % str(False and 1))
 print("extra 4. True and 1 = = %s" % str(True and 1))
-
